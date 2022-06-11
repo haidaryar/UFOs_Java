f184ea81f57f6cf41d2b070aaa7ff493c0bef54c
Waheed Haidaryar <74505893+haidaryar@users.noreply.github.com>
2022-06-11 15:09:49 -0700

    Date Search code added

--- static/js/app.js
+++ static/js/app.js
@@ -27,3 +27,40 @@ function buildTable(data) {
 });
 
 
+// search function
+
+function handleClick() {
+
+    d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+// check to see if the date was entered and filter data using the date
+
+if (date) { 
+
+    // apply filter to the table to only keep the rows where the datetime value matches the filter value 
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+
+
+
+}
+
+
+// Attach an event to listen for the form button
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+
+// Build the table when the page loads
+
+buildTable(tableData);
+
